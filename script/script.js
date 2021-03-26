15a6bfe3d8b54f1b6e670047f3331f714d4b2650
Oscar Najera <najerao87@gmail.com>
2021-03-26 10:11:51 -0600

    Version 01 punto de partida --se requiere eliminar los eventos al dar click en x imagen

--- script/script.js
+++ script/script.js
@@ -4,7 +4,8 @@ var img = document.getElementsByTagName("img");
 var btn = document.getElementById("btn");
 var contenedorImg= document.getElementById("contenedor-img");
 var imgcont=1;
-var activo=[];
+var activo=[];     //se usa para saber el nombre de la imagen que fue activada por el evento
+var eliminaEvento=[];//se usa para  saber cual evento se va a remover
 var contadorActivo=0;
 
 
@@ -32,18 +33,41 @@ function voltear(){
     reverso[0].style.transition = "0.4s linear";
 
     let temporal="img"+xclase.substr(3);//variable que rescata el numero la clase de la imagen 
-    console.log(temporal);
+    //let temporal2=parseInt(xclase.substr(3));
+    //console.log("temporal2 = "+temporal2);
     let temporalsrc = document.getElementsByClassName(temporal);//accedemos a la imagen atravez de la classe
     temporal=temporalsrc[0].getAttribute("src");                //extraemos el nombre de la imagen
-    temporal=temporal.substr(4 ,temporal.length-4 );             //se acorta el texpo para solo tener el nombre de la imagen
-    console.log(temporal);
+    tempora2=temporal.substr(4 ,temporal.length-4 );             //se acorta el texpo para solo tener el nombre de la imagen
+    console.log("nombre de img"+temporal);
+    eliminaEvento[contadorActivo]=xclase.substr(3)
+    activo[contadorActivo]=temporal;
 
-
- 
+    if (contadorActivo==1) {
+    	 validarPares(eliminaEvento[0],eliminaEvento[1], activo[0],activo[1]);
+    	 console.log('contadorActivo= '+contadorActivo)
+    }
+    else {
+    	contadorActivo++;
+    }
+    
 }
  
 
 
-function validarPares(){
+function validarPares(eventox,eventoy,valida1,valida2){
+	if (valida1==valida2) {
+		console.log("son pares");
+        carta_box[eventox].removeEventListener("click", voltear);
+        carta_box[eventoy].removeEventListener("click", voltear);
+
+		let elimina = document.querySelectorAll("img[src='img/vista.jpg']");
+		//elimina[0].style.width="500px"
+		//elimina[1].style.width="500px"
+
+	} 	
+	else {
+		console.log('no son pares');
+	}
+	contadorActivo=0;
 
 }
